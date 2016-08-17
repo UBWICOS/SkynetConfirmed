760b69b4175c3d97577d4ded6a6dd51988081f05
Đỗ Tiến Dũng <dungdo.sg@gmail.com>
2016-08-17 10:17:35 +0700

    finished

--- app/src/main/java/com/firefighter/skynetconfirmed/MainActivity.java
+++ app/src/main/java/com/firefighter/skynetconfirmed/MainActivity.java
@@ -2,6 +2,7 @@ package com.firefighter.skynetconfirmed;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.ContentObserver;
@@ -10,6 +11,8 @@ import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,58 +26,78 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
-
-
             requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
                     PackageManager.PERMISSION_GRANTED);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
         }
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.RECEIVE_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
-
-
             requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS},
                     PackageManager.PERMISSION_GRANTED);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
         }
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.READ_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
-
-
             requestPermissions(new String[]{Manifest.permission.READ_SMS},
                     PackageManager.PERMISSION_GRANTED);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
         }
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
-
-
             requestPermissions(new String[]{Manifest.permission.SEND_SMS},
                     PackageManager.PERMISSION_GRANTED);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
         }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PackageManager.PERMISSION_GRANTED);
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PackageManager.PERMISSION_GRANTED);
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},
+                    PackageManager.PERMISSION_GRANTED);
+        }
+
 
         ContentObserver observer = new MessageObserver(null, this);
         getContentResolver().registerContentObserver(
                 Uri.parse("content://sms"), true, observer);
+
+        final GestureDetector.SimpleOnGestureListener listener = new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                openSettings();
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+            }
+        };
+
+        final GestureDetector detector = new GestureDetector(listener);
+
+        detector.setOnDoubleTapListener(listener);
+        detector.setIsLongpressEnabled(true);
+
+        getWindow().getDecorView().setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                return detector.onTouchEvent(event);
+            }
+        });
+
     }
 
-    public void goToSettings(View view) {
+    public void openSettings() {
         Intent i = new Intent(this, SettingsActivity.class);
         startActivity(i);
     }
